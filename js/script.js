da5a0d112321211481a6b36c67319994c9af25c3
cif95 <vivianacifarelli9@gmail.com>
2022-03-08 16:56:18 +0100

    corrections + implemented bonus

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
-// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
-// - abbiamo calpestato una bomba
-// - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
-// BONUS:
-// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-// 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-
-
-
 const grid = document.getElementById('grid');
 const scoreOutput = document.getElementById('score');
 const levelSelectElement = document.getElementById('game-level');
 
 
 document.querySelector('#play-btn').addEventListener('click', function() {
-	addInnerHTMLContent(grid, '');
-	addInnerHTMLContent(scoreOutput, '');
 	generateGame();
 })
 
@@ -30,9 +16,11 @@ document.getElementById('reset-btn').addEventListener('click', function() {
 
 
 // ! FUNCTIONS below ! //
+
 /**
  * function that creates a div cell as a dom element
  * @param {*} innerContent inner html content of the div cell, wrapped by a span tag
+ * @param {*} cellsPerRow number of cells in a row
  * @returns a div cell element
  */
 function generateGridCellElement (innerContent, cellsPerRow) {
@@ -40,7 +28,7 @@ function generateGridCellElement (innerContent, cellsPerRow) {
 	gridCell.classList.add('cell');
 	gridCell.style.width = `calc(100% / ${cellsPerRow}`;
 	gridCell.style.height = gridCell.style.width;
-	addInnerHTMLContent(gridCell, `<span>${innerContent}</span>`);
+	addInnerHTMLContent(gridCell, `${innerContent}`);
 	return gridCell;
 }
 
@@ -50,13 +38,12 @@ function generateGridCellElement (innerContent, cellsPerRow) {
  * @returns a game board based on difficulty level selected
  */
 function generateGame(){
-
 	let isGameOver = false;
-
 	let cellsNumber;
 	let cellsPerRow;
 	let score = 0;
-	let cellsClicked = 0;
+	addInnerHTMLContent(grid, '');
+	addInnerHTMLContent(scoreOutput, '');
 	switch(parseInt(levelSelectElement.value)){
 		case 1 :
 			cellsNumber = 100;
@@ -76,21 +63,21 @@ function generateGame(){
 		const currentGridCell = generateGridCellElement(i, cellsPerRow);
 		currentGridCell.addEventListener('click', function() {
 			if( !isGameOver){
-				if (!mines.includes(i)) { // se la cella cliccata non è una mina
-					cellsClicked++;
+				if (!mines.includes(i)) { // if the clicked cell is not a mine..
 					score++;
 					addInnerHTMLContent(scoreOutput, `Your score is : ${score}`)
 					currentGridCell.classList.add('clicked');
-					if (cellsClicked == cellsNumber - 16){ // se si raggiunge il numero massimo possibile di numeri consentiti...
-						addInnerHTMLContent(scoreOutput, `Game over ! Your score is : ${score} points`);
-					}
-				} else { 
-					currentGridCell.classList.add('clicked-mine');
+				} else {
+					isGameOver = true; // otherwise game is over and..
 					addInnerHTMLContent(scoreOutput, `Game over ! Your score is : ${score} points`);
-					isGameOver = true;
+					for ( let i = 0; i < grid.children.length ; i++){ // every cell, which is a mine, ... explodes!
+						if( mines.includes(parseInt(grid.children[i].innerHTML)) ) {
+							grid.children[i].innerHTML = `<img class="bg-danger" src="img/bomb.png" alt="bomb image">`;
+						}
+					}
 				}
 			}
-		})
+		}, { once : true });
 		grid.appendChild(currentGridCell);
 	}
 }
@@ -106,13 +93,13 @@ function generateGame(){
 function generateUniqueRandomInt (blacklist, min, max) {
 	let control = false;
 	let uniqueRandomInt;
-	while ( control == false ){
-		uniqueRandomInt  = Math.floor(Math.random() * (max - min) + min);
+	while ( !control ){
+		uniqueRandomInt  = Math.floor(Math.random() * ((max + 1) - min) + min);
 		if ( !blacklist.includes(uniqueRandomInt)  ){
 			control = true;
 		}
-	return uniqueRandomInt;
 	}
+	return uniqueRandomInt;
 }
 
 
@@ -125,7 +112,7 @@ function generateUniqueRandomInt (blacklist, min, max) {
 function generateMines (minesNumber, cellsNumber) {
 	const minesList = [];
 	for ( let i = 0; i < minesNumber; i++){
-		minesList.push(generateUniqueRandomInt(minesList, 0, cellsNumber));
+		minesList.push(generateUniqueRandomInt(minesList, 1, cellsNumber));
 	}
 	return minesList;
 }
@@ -140,6 +127,3 @@ function generateMines (minesNumber, cellsNumber) {
 function addInnerHTMLContent (DOMElement, content) {
 	return DOMElement.innerHTML = content;
 }
-
-
-
